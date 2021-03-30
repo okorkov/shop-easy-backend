43e2db152abde0885f70a7a14f563262ed85a3c0
okorkov <alex.okorkov@gmail.com>
2021-03-30 14:48:06 -0700

    added quantity column to the products schema

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -6,7 +6,9 @@ class Api::CategoriesController < ApplicationController
 
   def show
     category = Category.find_by(id: params[:id])
-    render json:  category, only: [:id, :name], include: [:products]
+    render json:  category, only: [:id, :name], include: [:products => {except: [:created_at, :updated_at]}]
   end
 
 end
+
+# render json: product, except: [:created_at, :updated_at, :category_id], include: [:category => {only: [:name, :id]}]
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,12 @@
 class Api::ProductsController < ApplicationController
 
   def index
-    render json: Product.all
+    render json: Product.all, except: [:created_at, :updated_at]
   end
 
   def show
     product = Product.find_by(id: params[:id])
-    render json: product
+    render json: product, except: [:created_at, :updated_at, :category_id], include: [:category => {only: [:name, :id]}]
   end
 
 end
