9899d100323eae484828632db2ff1c292292aaf9
okorkov <alex.okorkov@gmail.com>
2021-04-02 11:45:43 -0700

    encoded users information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,40 @@
 class ApplicationController < ActionController::API
+  JWT_SECRET = ENV['JWT_SECRET']
+
+    # Encode / Decode
+    def encode_token(payload)
+        JWT.encode(payload, JWT_SECRET)
+    end
+
+    def auth_header # {Authorization: 'Bearer <token>'}
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, JWT_SECRET, true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    #Authentication Helper methods
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    #Authorization Helper methods
+    def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,22 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user
+    user_encoded = encode_token({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      admin: user.admin,
+      current_order: user.current_order
+    })
+    if user 
+      render json: {user_token: user_encoded}
+    else
+      render json: {message: 'User not found'}
+    end
+  end
+
+  def create
+    raise params.inspect
   end
 
 end
