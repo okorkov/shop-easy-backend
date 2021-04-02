ff931ada2c1a8078ff07a4c168b4696b30162790
okorkov <alex.okorkov@gmail.com>
2021-04-02 16:29:48 -0700

    added user creation controller with sessions assigned

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: {status: 'authorized', logged_in: true, user: user}
+      render json: {status: 200, logged_in: true, user: user}
     else
       render json: { errors: ['Invalid username or password'], status: 401}
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: {status: 200, user: user}
+    else
+      render json: {status: 501, message: user.errors.full_messages}
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :name, :password, :password_confirmation)
   end
 
 end
