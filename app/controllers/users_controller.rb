1ff76baef1089595e2d8d7069218c8d6227d6182
okorkov <alex.okorkov@gmail.com>
2021-04-02 15:00:31 -0700

    JWT set up removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,18 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    user_encoded = encode_token({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      admin: user.admin,
-      current_order: user.current_order
-    })
     if user 
-      render json: {user_token: user_encoded}
+      render json: user
     else
       render json: {message: 'User not found'}
     end
   end
 
   def create
+    raise params.inspect
+  end
+
+  def user_auth
     raise params.inspect
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  post 'users/user_auth'
 
   resources :users, except: [:index]
 
