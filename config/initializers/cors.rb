f74b0ab278333c02943d63dabd11913af9f1710b
okorkov <alex.okorkov@gmail.com>
2021-04-02 19:06:43 -0700

    cors settings change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,16 +18,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
-  allow do
-    origins 'http://localhost:3001' 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+  # allow do
+  #   origins 'http://localhost:3001' 
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
 
   allow do
-    origins 'https://shop-easy-online.web.app/'
+    origins 'https://shop-easy-online.web.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
