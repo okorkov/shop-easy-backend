690564d24d28389fec0c7fabc339c6471ec6c8ee
okorkov <alex.okorkov@gmail.com>
2021-04-04 17:35:23 -0700

    cors changed back for local host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,14 +18,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
-  # allow do
-  #   origins 'http://localhost:3001' 
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
-
+  allow do
+    origins 'http://localhost:3001' 
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+# remove local host before deployment to heroku
   allow do
     origins 'https://shop-easy-online.web.app'
     resource '*',
