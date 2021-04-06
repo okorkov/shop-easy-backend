a5408a0e8afa05c97cf153817468d2b288df179f
okorkov <alex.okorkov@gmail.com>
2021-04-06 09:30:04 -0700

    changed order belongs to user to be optional

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,14 +2,16 @@ class Api::OrdersController < ApplicationController
 
   def create
     if current_user
-      order = Order.new
-      current_user.orders << order
-      order.save
-      render json: current_user,include: [:orders]
+      # order = Order.new
+      # current_user.orders << order
+      # order.save
+      # render json: current_user,include: [:orders]
+      render json: {params: params}
     else
-      order = Order.new
-      order.save
-      render json: order,include: [:order_items]
+      # order = Order.new
+      # order.save
+      # render json: order,include: [:order_items]
+      render json: {text: 'NOT current user'}
     end
   end
 
