eb7ffbc26130f3de56ab4b730d25347fb01a7330
okorkov <alex.okorkov@gmail.com>
2021-04-06 19:58:32 -0700

    cleared carts controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -20,17 +20,3 @@ class Api::CartsController < ApplicationController
 
 end
 
-# t.integer "quantity"
-# t.integer "product_id"
-# t.integer "shopping_cart_id"
-# t.decimal "total", precision: 10, scale: 2
-# t.decimal "unit_price", precision: 10, scale: 2
-  params = {"data"=>
-    {"id"=>1, "name"=>"Iphone 11 pro", "description"=>"Apples products bhal blah 3 cameras blah blah...", "price"=>"999.99", "category_id"=>1, "updated_at"=>"2021-03-31T18:00:00.991Z", "quantity"=>10, "image"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IUypKEvG1vaD0CLQQ5CpLDEXlQSQcvfc5Q&usqp=CAU"},
-    "user"=>{"logged_in"=>true, "user"=>{"id"=>1, "name"=>"Alex", "email"=>"alex.okorkov@gmail.com", "password_digest"=>"[FILTERED]", "admin"=>true, "current_order"=>3, "created_at"=>"2021-03-30T18:51:11.927Z", "updated_at"=>"2021-04-06T16:28:12.182Z"}, 
-    "currentCart"=>{"id"=>9, "total"=>nil, "subtotal"=>nil, "created_at"=>"2021-04-06T19:41:10.099Z", "updated_at"=>"2021-04-06T19:41:10.099Z"}, 
-    "currentItems"=>[]}, 
-    "cart"=>{"data"=>{"id"=>1, "name"=>"Iphone 11 pro", "description"=>"Apples products bhal blah 3 cameras blah blah...", "price"=>"999.99", "category_id"=>1, "updated_at"=>"2021-03-31T18:00:00.991Z", "quantity"=>10, "image"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IUypKEvG1vaD0CLQQ5CpLDEXlQSQcvfc5Q&usqp=CAU"},
-     "user"=>{"logged_in"=>true, "user"=>{"id"=>1, "name"=>"Alex", "email"=>"alex.okorkov@gmail.com", "password_digest"=>"[FILTERED]", "admin"=>true, "current_order"=>3, "created_at"=>"2021-03-30T18:51:11.927Z", "updated_at"=>"2021-04-06T16:28:12.182Z"}, 
-     "currentCart"=>{"id"=>9, "total"=>nil, "subtotal"=>nil, "created_at"=>"2021-04-06T19:41:10.099Z", "updated_at"=>"2021-04-06T19:41:10.099Z"}, 
-     "currentItems"=>[]}}}
