96e439b8e482038cef9940290403fe7e36141d00
okorkov <alex.okorkov@gmail.com>
2021-04-09 15:54:00 -0700

    config reverted back heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::API
 
-  # before_action :authorized
-  
-
   include ActionController::Cookies
-
+  
+  # skip_before_action :verify_authenticity_token
+  # before_action :authorized
 
     def current_user
         @current_user = User.find_by(id: session[:user_id])
